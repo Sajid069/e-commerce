1ea463fd583e51b5d6e397c84b480208a7104dc6
Sajid069 <mdsajidhossainkhan2@gmail.com>
2024-03-05 15:03:45 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shop/app.dart';
+import 'package:shop/utilis/constants/colors.dart';
+import 'package:shop/utilis/theme/theme.dart';
+
+void main() {
+
+  //Todo: Add Widgets Binding
+  //Todo: Init Local Storage
+  //Todo: Await Native Splash
+  //Todo: Initialize Firebase
+  //Todo: Initialize Authentication
+
+  
+
+  runApp(const App());
+}
+
+
---
+++ lib/utilis/constants/text_strings.dart
@@ -0,0 +1,19 @@
+
+
+class TTexts{
+  
+  // -- GLOBAL Texts
+
+  // -- OnBoarding Texts
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Delicer at your door step";
+
+  static const String onBoardingSubtitle1 = "Welcome to a World of Limitless Choices = Your Perfect Product Awaits!";
+  static const String onBoardingSubtitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBoardingSubtitle3 = "From Our Doorstem to Yours - Swift, Secure, and Contactless Delivery!";
+
+  // -- Home
+  static const String homeTitle = "Good day for shopping";
+  static const String homeSubtitle = "Udaam kenakata";
+}
